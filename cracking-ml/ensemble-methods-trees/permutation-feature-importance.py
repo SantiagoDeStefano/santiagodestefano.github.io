f8d022cdfd70c6c2f443bf947b40cba8a0f9b7b8
Phạm Khôi Nguyên <ngpham.2807@gmail.com>
2026-07-17 17:17:02 +0700

    hyphen to underscore

---
+++ cracking-ml/ensemble-methods-trees/permutation-feature-importance.py
@@ -0,0 +1,13 @@
+from typing import Callable, Optional
+import numpy as np
+
+
+def permutation_importance(
+        model, 
+        X: np.array, 
+        y: np.array,
+        n_repeats = 10,
+        random_state = 42
+    ):
+    rng = np.random.default_rng(random_state)
+    baseline = metric(y, model.predict(X))
